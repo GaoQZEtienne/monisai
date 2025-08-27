2058abebfc499aacfdc3c9ed60c01f8fe2ec819f
GaoQZEtienne <gao.qianzhi-etienne@centralepekin.cn>
2025-08-27 22:20:45 +0800

    Add files via upload

--- summary_sales.py
+++ summary_sales.py
@@ -13,7 +13,11 @@ df["月份"] = df["销售日期"].dt.to_period("M")
 
 summary = df.groupby(["分类编码", "分类名称", "单品编码", "单品名称", "月份"], as_index=False)["销量(千克)"].sum()
 
-# 写入
+#按出现次数排序
+summary["出现次数"] = summary.groupby("单品编码")["月份"].transform("count")
+summary = summary.sort_values(by=["出现次数", "单品编码"], ascending=[False, True])
+
+#写入
 with pd.ExcelWriter("summary.xlsx", engine="openpyxl") as writer:
     for pinlei, pinleiGroup in summary.groupby(["分类编码", "分类名称"]):
         sheetName = str(pinlei[1])
